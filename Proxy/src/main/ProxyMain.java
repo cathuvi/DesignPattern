941b2a66e4533e4501a1cdd819134302d271d908
ts.youn <ts.youn@DESKTOP-G1M17PT>
2021-05-15 13:13:27 +0900

    Project별 정리 Proxy 패턴 Aop 예제

---
+++ Proxy/src/main/ProxyMain.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import aop.AopBrowser;
+import proxy.BrowserProxy;
+import proxy.IBrowser;
+
+public class ProxyMain {
+	public static void main(String[] args) {
+//		Browser browser = new Browser("www.naver.com");
+//		browser.show();
+//		browser.show();
+//		browser.show();
+//		browser.show();
+		
+//		IBrowser browser = new BrowserProxy("www.naver.com");
+//		browser.show();
+//		browser.show();
+//		browser.show();
+//		browser.show();
+//		browser.show();
+		
+		AtomicLong start = new AtomicLong();
+		AtomicLong end = new AtomicLong();
+		
+		IBrowser aopBrowser = new AopBrowser("www.naver.com",
+				()->{
+					System.out.println("before");
+					start.set(System.currentTimeMillis());
+				},
+				()->{
+					long now = System.currentTimeMillis();
+					end.set(now - start.get());
+				}
+				
+		);
+		
+		aopBrowser.show();
+		System.out.println("loading time :"+end.get());
+		
+		aopBrowser.show();
+		System.out.println("loading time :"+end.get());
+	}
+}
